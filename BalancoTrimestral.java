150e93efef65669ac25a5cafc43e3d054c846efc
xKaue007 <kaueoliveira92@gmail.com>
2021-08-19 18:39:37 +0000

    Balaço

---
+++ BalancoTrimestral.java
@@ -0,0 +1,12 @@
+public class BalancoTrimestral{
+    public satatic void main (String args[]){
+        int gastosJaneiro=15000;
+        int gastosFevereiro=22000;
+        int gastosMarco=17000;
+        int gastosTrimestral=gastosJaneiro+gastosFevereiro+gastosMarco;
+        int gastosMensal=gastosTrimestral/3;
+
+        System.out.println("O gasto Trimestral foi de : "+gastosTrimestral);
+        System.out.println("O gasto Mensal foi de: "+gastosMensal);
+    }
+}
